543cbd1499c33a9f6bdbe8cbd1649b5e02aa8f54
ruigames <rui_games@hotmail.com>
2015-03-18 10:54:22 +0000

    bug corrected

--- TowerDeFeup/Assets/Scripts/EnemySpawner.cs
+++ TowerDeFeup/Assets/Scripts/EnemySpawner.cs
@@ -5,30 +5,7 @@ public class EnemySpawner : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		var tex = new Texture2D(20, 20);
 
-		var y = 10;
-		var d = 1/4 - 10;
-		var end = Mathf.Ceil(10/Mathf.Sqrt(2));
-		Color col = Color (Random.Range (0.25, 1.0), Random.Range (0.25, 1.0), Random.Range (0.25, 1.0));
-		int cy = 2, cx = 2;
-		for (int x = 0; x <= end; x++) {
-			tex.SetPixel(cx+x, cy+y, col);
-			tex.SetPixel(cx+x, cy-y, col);
-			tex.SetPixel(cx-x, cy+y, col);
-			tex.SetPixel(cx-x, cy-y, col);
-			tex.SetPixel(cx+y, cy+x, col);
-			tex.SetPixel(cx-y, cy+x, col);
-			tex.SetPixel(cx+y, cy-x, col);
-			tex.SetPixel(cx-y, cy-x, col);
-			
-			d += 2*x+1;
-			if (d > 0) {
-				d += 2 - 2*y--;
-			}
-		}
-
-		tex.Apply();
 
 	}
 	
