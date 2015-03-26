40f70167d9013b6d8a22bdbd3d1910f52b09e7d1
Freixo <hugo1245@gmail.com>
2015-03-26 17:11:38 +0000

    Initial Menu Added

---
+++ TowerDeFeup/Assets/Scripts/Start.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Start : MonoBehaviour {
+
+	public GameObject LoadingImage;
+	public GameObject LoadingText;
+
+	public void LoadScene(int level){
+		LoadingImage.SetActive (true);
+		LoadingText.SetActive (true);
+		Application.LoadLevel(level);
+	}
+}
