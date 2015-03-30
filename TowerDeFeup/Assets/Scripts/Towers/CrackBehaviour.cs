91f0b0c30b18e5474de7cab0ecd80197cdc0d1fa
ei12159 <ei12159@fe.up.pt>
2015-03-30 18:29:15 +0100

    Minas Tower added

---
+++ TowerDeFeup/Assets/Scripts/Towers/CrackBehaviour.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrackBehaviour : MonoBehaviour {
+	
+	public GameObject enemy;
+	public float speed;
+	
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float step = speed * Time.deltaTime;
+		StartCoroutine(FadeTo(0.0f, 3.0f));
+	}
+	
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Enemy") {
+			EnemyBehaviour behaviour=other.gameObject.GetComponent<EnemyBehaviour> ();
+			behaviour.health = other.gameObject.GetComponent<EnemyBehaviour> ().health - 200;
+			if(behaviour.health<=0)
+				Destroy (other.transform.parent.gameObject);
+		}
+		
+	}
+
+	public IEnumerator FadeTo(float aValue, float aTime)
+	{
+		float alpha = this.GetComponent<Renderer>().material.color.a;
+		for (float t = 0.0f; t < 1.0f; t += Time.deltaTime / aTime)
+		{
+			Color newColor = new Color(1, 1, 1, Mathf.Lerp(alpha,aValue,t));
+			this.GetComponent<Renderer>().material.color = newColor;
+			yield return null;
+		}
+	}
+	
+}
---
+++ TowerDeFeup/Assets/Scripts/Towers/LCEEMGTowerBehaviour.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LCEEMGTowerBehaviour : MonoBehaviour
+{
+	
+	private TowerRangeBehaviour rangeScript;
+	public GameObject range;
+	private List<GameObject> enemies = new List<GameObject> ();
+	private GameObject shootedEnemy;
+	public float speed;
+	public GameObject crack;
+	private GameObject crack2;
+	public float distance;
+	public float width, height;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		
+		getEnemies ();
+		Invoke("CreateBullet",0.1f);
+		
+		rangeScript = range.GetComponent<TowerRangeBehaviour> ();
+		rangeScript.setDistance (distance);
+		rangeScript.setPosition (transform);
+	}
+	
+	void getEnemies ()
+	{
+		enemies = new List<GameObject> ();
+		
+		foreach (GameObject enemy in GameObject.FindGameObjectsWithTag("Enemy")) {
+			enemies.Add (enemy);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+		getEnemies ();
+		
+		if (enemies.Count > 0) {
+			GetNearestEnemy ();
+		}
+	}
+	
+	void OnMouseEnter ()
+	{
+		rangeScript.setRange (true);
+	}
+	
+	void OnMouseExit ()
+	{
+		rangeScript.setRange (false);
+	}
+	
+	public void CreateBullet ()
+	{
+		if (enemies.Count > 0) {
+			if (Vector3.Distance (shootedEnemy.transform.position, transform.position) <= distance) {
+				crack.GetComponent<CrackBehaviour> ().enemy = shootedEnemy;
+				crack2 = Instantiate (crack, transform.position, Quaternion.identity)as GameObject;
+			}
+		}
+		Invoke("CreateBullet",3);
+	}
+	
+	void GetNearestEnemy ()
+	{
+		float nearestDistanceSqr = Mathf.Infinity;
+		GameObject nearestObj = null;
+		
+		// loop through each tagged object, remembering nearest one found
+		foreach (GameObject enemy in enemies) {
+			Vector3 enemyPos = enemy.transform.position;
+			float distanceSqr = Vector3.Distance(enemyPos, transform.position);
+			
+			if (distanceSqr < nearestDistanceSqr) {
+				nearestObj = enemy;
+				nearestDistanceSqr = distanceSqr;
+			}
+		}
+		shootedEnemy = nearestObj;
+	}
+}
+
