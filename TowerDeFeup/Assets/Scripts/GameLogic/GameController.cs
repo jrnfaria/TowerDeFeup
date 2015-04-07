133ebc359b700d61a7bd5c03439e4c05398a433a
ruigames <rui_games@hotmail.com>
2015-04-07 15:54:44 +0100

    enemies give money now
    yay!!!See gameController for more

---
+++ TowerDeFeup/Assets/Scripts/GameLogic/GameController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	private int money;
+	public GUIText moneyText;
+
+	// Use this for initialization
+	void Start () {
+		money = 0;
+		moneyText.text = "Money:"+money;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		moneyText.text = "Money:"+money;
+	}
+
+	public void addMoney(int m)
+	{
+		money = m + money;
+	}
+}
