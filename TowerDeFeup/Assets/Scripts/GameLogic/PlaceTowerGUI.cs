49cb96631ca965d86052f706a540af3b71827966
ruigames <rui_games@hotmail.com>
2015-04-09 16:17:35 +0100

    interface improved gameover works 1%

--- TowerDeFeup/Assets/Scripts/GameLogic/PlaceTowerGUI.cs
+++ TowerDeFeup/Assets/Scripts/GameLogic/PlaceTowerGUI.cs
@@ -2,49 +2,65 @@
 using System.Collections;
 
 public class PlaceTowerGUI : MonoBehaviour {
-
+	
 	private Vector3 wantedPos;
 	private TileBehaviour tileScript;
 	public Texture2D textureMIEIC;
 	public Texture2D textureMIEEC;
 	public Texture2D textureLCEEMG;
 	public Texture2D textureMIEQ;
-
+	
 	// Use this for initialization
 	void Start () {
-		wantedPos = Camera.main.WorldToViewportPoint (transform.position);
+		wantedPos = Camera.main.WorldToScreenPoint(transform.position);
+		wantedPos = new Vector3 (wantedPos.x, Screen.height-wantedPos.y , wantedPos.z);
 		tileScript = GetComponent<TileBehaviour>();
 	}
-	
 	// Update is called once per frame
 	void Update () {
-	
-	}
-
-	void OnGUI () {
-		// Make a background box
-		GUI.Box(new Rect(wantedPos.x,wantedPos.y,100,100),"");
 		
-		GUI.DrawTexture(new Rect(wantedPos.x,wantedPos.y,45,45) , textureMIEIC);
-		if(GUI.Button(new Rect(wantedPos.x+10,wantedPos.y+20,80,20), "", new GUIStyle())) {
+	}
+	
+	void OnGUI () {
+		float rx = Screen.width / 1280.0f; //or whatever with do you want;
+		float ry = Screen.height / 720.0f; //or whatever height do you want;
+		
+		float buttonWidht = 50 * rx;
+		float buttonHeight = 50 * ry;
+		
+		float offsetX1 = 75 * rx;
+		float offsetX2 = 25 * rx;
+		
+		float offsetY1 = 75 * ry;
+		float offsetY2 = 25 * ry;
+		
+		// Make a background box
+		GUI.Box(new Rect(wantedPos.x-offsetX1,wantedPos.y-offsetY2,buttonWidht,buttonHeight),"");
+		GUI.Box(new Rect(wantedPos.x+offsetX2,wantedPos.y-offsetY2,buttonWidht,buttonHeight),"");
+		GUI.Box(new Rect(wantedPos.x-offsetX2,wantedPos.y-offsetY1,buttonWidht,buttonHeight),"");
+		GUI.Box(new Rect(wantedPos.x-offsetX2,wantedPos.y+offsetY2,buttonWidht,buttonHeight),"");
+		
+		
+		GUI.DrawTexture(new Rect(wantedPos.x-offsetX1,wantedPos.y-offsetY2,buttonWidht,buttonHeight) , textureMIEIC);
+		if(GUI.Button(new Rect(wantedPos.x-offsetX1,wantedPos.y-offsetY2,buttonWidht,buttonHeight), "", new GUIStyle())) {
 			tileScript.setTowerPlacement(1);
 			enabled=false;
 		}
 		
-		GUI.DrawTexture(new Rect(wantedPos.x+55,wantedPos.y,45,45) , textureMIEEC);
-		if(GUI.Button(new Rect(wantedPos.x+55,wantedPos.y,45,45), "", new GUIStyle())) {
+		GUI.DrawTexture(new Rect(wantedPos.x+offsetX2,wantedPos.y-offsetY2,buttonWidht,buttonHeight) , textureMIEEC);
+		if(GUI.Button(new Rect(wantedPos.x+offsetX2,wantedPos.y-offsetY2,buttonWidht,buttonHeight), "", new GUIStyle())) {
 			tileScript.setTowerPlacement(2);
 			enabled=false;
 		}
 		
-		GUI.DrawTexture(new Rect(wantedPos.x,wantedPos.y+55,45,45) , textureLCEEMG);
-		if(GUI.Button(new Rect(wantedPos.x,wantedPos.y+55,45,45), "", new GUIStyle())) {
+		GUI.DrawTexture(new Rect(wantedPos.x-offsetX2,wantedPos.y-offsetY1,buttonWidht,buttonHeight) , textureLCEEMG);
+		if(GUI.Button(new Rect(wantedPos.x-offsetX2,wantedPos.y-offsetY1,buttonWidht,buttonHeight), "", new GUIStyle())) {
 			tileScript.setTowerPlacement(3);
 			enabled=false;
 		}
 		
-		GUI.DrawTexture(new Rect(wantedPos.x+55,wantedPos.y+55,45,45) , textureMIEQ);
-		if(GUI.Button(new Rect(wantedPos.x+55,wantedPos.y+55,45,45), "MIEQ", new GUIStyle())) {
+		GUI.DrawTexture(new Rect(wantedPos.x-offsetX2,wantedPos.y+offsetY2,buttonWidht,buttonHeight) , textureMIEQ);
+		if(GUI.Button(new Rect(wantedPos.x-offsetX2,wantedPos.y+offsetY2,buttonWidht,buttonHeight), "", new GUIStyle())) {
 			tileScript.setTowerPlacement(4);
 			enabled=false;
 		}
